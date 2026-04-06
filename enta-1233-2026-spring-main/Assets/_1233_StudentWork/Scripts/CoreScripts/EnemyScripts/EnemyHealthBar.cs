b16fd50b253fa745ba154a92bea1cfec825b58df
Epicjeong <yurijeong@icloud.com>
2026-04-06 16:44:15 -0700

    checkpoint

--- enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/CoreScripts/EnemyScripts/EnemyHealthBar.cs
+++ enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/CoreScripts/EnemyScripts/EnemyHealthBar.cs
@@ -4,38 +4,8 @@ using UnityEngine.UI;
 
 public class EnemyHealthBar : MonoBehaviour
 {
-    private Vector2 positionCorrection = new Vector2(0, 100);
+    //Shader EnemyHP()
+    //{
 
-    [SerializeField] private RectTransform targetCanvas;
-    [SerializeField] private RectTransform healthBar;
-    [SerializeField] private Transform objectToFollow;
-
-    public void SetHealthBarData(Transform targetTransform, RectTransform healthBarPanel)
-    {
-        this.targetCanvas = healthBarPanel;
-        healthBar = GetComponent<RectTransform>();
-        objectToFollow = targetTransform;
-        RepositionHealthBar();
-        healthBar.gameObject.SetActive(true);
-    }
-
-    public void OnHealthChanged(float healthFill)
-    {
-        healthBar.GetComponent<Image>().fillAmount = healthFill;
-    }
-
-    void Update()
-    {
-        RepositionHealthBar();
-    }
-
-    private void RepositionHealthBar()
-    {
-        Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(objectToFollow.position);
-        Vector2 WorldObject_ScreenPosition = new Vector2(
-        ((ViewportPosition.x * targetCanvas.sizeDelta.x) - (targetCanvas.sizeDelta.x * 0.5f)),
-        ((ViewportPosition.y * targetCanvas.sizeDelta.y) - (targetCanvas.sizeDelta.y * 0.5f)));
-        
-        healthBar.anchoredPosition = WorldObject_ScreenPosition;
-    }
+    //}
 }
